f29f84a17b57af0fda8a3af2457e27eb9093e56d
dushyant1414 <dushyant14@gmail.com>
2017-08-27 15:13:12 +0800

    Add files via upload
    Updated main Run_Analysis file

---
+++ Run_analysis.R
@@ -0,0 +1,41 @@
+#import necessary packages
+install.packages("reshape2")
+library(reshape2)
+
+# Load activity labels + features
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
+
+# Extract only the data on mean and standard deviation
+filtered <- grep(".*mean.*|.*std.*", features[,2])
+filtered.names <- features[filtered,2]
+filtered.names = gsub('-mean', 'Mean', filtered.names)
+filtered.names = gsub('-std', 'Std', filtered.names)
+filtered.names <- gsub('[-()]', '', filtered.names)
+
+
+# Load the datasets
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[filtered]
+trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
+
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[filtered]
+testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
+
+# merge datasets and add labels
+allData <- rbind(train, test)
+colnames(allData) <- c("subject", "activity", filtered.names)
+
+# turn activities & subjects into factors
+allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+allData$subject <- as.factor(allData$subject)
+
+allData.melted <- melt(allData, id = c("subject", "activity"))
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
+
+write.table(allData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
